e41e56b7ab317d57dfcf333da8fa79b24324848b
LeBronLiHD <lebronlihd@outlook.com>
2022-03-09 16:29:39 +0800

    update introduction-configuration

---
+++ 0-introduction/test_configuration.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+from IPython import display
+from torchvision.datasets import MNIST, FashionMNIST
+from torchvision import transforms
+import time
+
+
+def show_image(path):
+    print(path)
+    img = cv2.imread(path)
+    cv2.imshow("kitty", img)
+    key = cv2.waitKey(0)
+    if key == 'q' or key == '27':
+        cv2.destroyAllWindows()
+
+
+def download_mnist():
+    transform = transforms.Compose([transforms.ToTensor()])
+    data_train = MNIST(root="../data/",
+                       transform=transform,
+                       train=True,
+                       download=True)
+    data_test = MNIST(root="../data/",
+                      transform=transform,
+                      train=False)
+
+
+if __name__ == '__main__':
+    download_mnist()
+    show_image("../images/cat.jpg")
